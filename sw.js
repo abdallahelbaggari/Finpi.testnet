4e6a57a948c75ec1e0276fec34f55faa0f22d104
abdallahelbaggari <abdallahelbaggari@gmail.com>
2026-08-05 14:11:59 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,17 @@
+/* FinPi SW v10-testnet - deletes all caches and unregisters */
+var CACHE = 'finpi-testnet-v10';
 self.addEventListener('install', function(){ self.skipWaiting(); });
 self.addEventListener('activate', function(e){
   e.waitUntil(
-    caches.keys().then(function(ks){ return Promise.all(ks.map(function(k){ return caches.delete(k); })); })
-      .then(function(){ return self.registration.unregister(); })
+    caches.keys().then(function(keys){
+      return Promise.all(keys.map(function(k){ return caches.delete(k); }));
+    }).then(function(){
+      return self.registration.unregister();
+    }).then(function(){
+      return self.clients.claim();
+    })
   );
+});
+self.addEventListener('fetch', function(e){
+  e.respondWith(fetch(e.request));
 });
